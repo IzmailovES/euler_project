1472eb45c8c04639031ca898a999fed6cd0965ec
Egor Izmailov <e.izmailov@nicevt.ru>
2023-09-27 11:51:58 +0300

    problem 439 with factor result 1m10s for 10**5

--- problem439.py
+++ problem439.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
 
-def d(num): ## sum of all divisors
+import primes
+
+def factorize(num):
+    if not num:
+        return {1:1}
+    acc = dict()
+    p = primes.Primes()
+    pp = 0
+    while num != 1:
+        prime = p[pp]
+        if not (num % prime):
+            acc[prime] = acc.get(prime, 0) + 1
+            num //= prime
+        else:
+            pp += 1
+
+    return acc
+
+
+def d_old(num): ## sum of all divisors
     ret = 0
     for i in range(1,(num+1)>>1):
         if not ( num%i):
             ret += i
     return ret
+
+def d(num):
+    delimers = factorize(num)
+
 
 def s(n):
     cache = dict()
@@ -16,5 +39,10 @@ def s(n):
             ret += d(i*j)
     return ret
 
-print(s(200))
+
+
+for i in range(10**5):
+    print(i,factorize(i))
+print(primes.Primes()[0])
+#print(s(200))
 
