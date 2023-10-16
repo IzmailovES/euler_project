d7d11c49e36ab35ad3b0849cd7b81b6b79b53dae
Egor Izmailov <e.izmailov@nicevt.ru>
2023-10-16 14:57:01 +0300

    little improve

--- problem439.py
+++ problem439.py
@@ -30,12 +30,13 @@ def factorize_rec(max_cachesize = 500**2):
             return {num:1}
         # если находим в кеше - возвращаем
         if num <= max_cachesize and num in cache:
-            return cache[num]
+            return dict(cache[num])
         # пытаемся поделить на простые числа по  очереди до корня из себя:
         acc = dict()
         lim = int(math.sqrt(num)) + 1
         while num != 1:
-            prime = primes.Primes()[pp]
+            #prime = primes.Primes()[pp]
+            prime = primes.Primes.known[pp]
             if prime > lim and s_num == num:
                 return {s_num:1}
             if not (num % prime):
@@ -51,6 +52,7 @@ def factorize_rec(max_cachesize = 500**2):
         return acc
     return inner
 f = factorize_rec()
+
 
             #  удалось поделить - делим на делитель сколько можем - возвращаем дикт сумм делителя + вызов себя от остатка c указанием следующего простого числа
 
@@ -75,7 +77,7 @@ def multi_d(dct):
             pass
     return ret
 
-def multiple_delimers(d1,d2):
+def multiple_delimers(d1,d2, pr = False):
     d3 = dict()
     ks = d1.keys()
     for i in ks:
@@ -88,9 +90,23 @@ def balance_d(d1,d2):
     ret2 = dict()
     for x,y in d2.items():
         if x in d1:
-            ret2[x] = d1[x] + d2[x]
+            ret2[x] = d1[x] + y
             del ret1[x]
-    return ret1,ret2
+        else:
+            ret2[x] = y
+    return multi_d(ret1) * multi_d(ret2)
+    #return ret1, ret2
+
+def balance_d_(d1,d2):
+    ret1 = dict(d1)
+    it = d2.items()
+    for x,y in it:
+        if x in d1:
+            d2[x] += d1[x]
+            del ret1[x]
+   # print(d2)
+   # print(multi_d(d2))
+    return multi_d(ret1) * multi_d(d2)
 
 def balance_i(i,j):
     print(i,j)
@@ -103,8 +119,9 @@ def balance_i(i,j):
         j *= nod
     return i,j
 
-print(balance_i(3*3*3*5*7*11*11,5*5*3*13))
-exit(0)
+#print(balance_d({3:4, 5:2, 11:1,7:1, 13:2},{3:2, 5:4,7:1, 17:1}))
+#print(balance_d_({3:4, 5:2,7:1, 11:1, 13:2},{3:2, 5:4,7:1, 17:1}))
+#exit(0)
 
 def s(n):
     cache = dict()
@@ -137,20 +154,30 @@ def s(n):
 def s3(n):
     ret1 = 0
     ret2 = 0
+    k = 10**9
     for i in range(1,n+1):
         d1 = f(i)
         dd1 = multi_d(d1)
         ret1 += multi_d(dict_summ(d1,d1))
+        ret1 %=k
         ret3 = 0
         for j in range(1,i):
             d2 = f(j)
+            #print(j,d2)
             nod = primes.nod(i, j)
             if nod == 1:
                 ret3 += multi_d(d2)
             else:
-                ret2 += multiple_delimers(d1, d2)
-        ret2 += ret3*dd1
-    return ret1 + (ret2<<1)
+                #print(i,j,d1,d2,ret2)
+                ret2 += balance_d_(d1,d2)%k
+                #print(ret2)
+                #ret2 += multi_d(d11) * multi_d(d22)
+                #ret2 += multiple_delimers(d1, d2)
+        ret2 += (ret3*dd1)
+        ret2 %=k
+        if not i%1000:
+            print(i, time.process_time())
+    return (ret1 + (ret2<<1))%k
 
 
 def factor_exam(num,d):
@@ -163,14 +190,16 @@ def factor_exam(num,d):
 
 if __name__ == '__main__':
     num = int(sys.argv[1])
-    prime_lim = int(math.sqrt(num)) + 1
+    prime_lim = int(math.sqrt(num)+1000) + 1
     print(f'inventing primes until {prime_lim}')
     n = 0
     while primes.Primes()[n] < prime_lim:
         n+=1
     print('go calculte!', time.process_time())
+    #for i in range(num):
+    #    print(i, f(i))
     strt = time.process_time()
-    print(s(num), time.process_time() - strt)
+    #print(s(num), time.process_time() - strt)
     strt = time.process_time()
     print(s3(num), time.process_time() - strt)
     exit(0)
