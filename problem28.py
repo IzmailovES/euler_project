914a3263135b2fad6415cb12488cfd8d83c61b79
Egor Izmailov <e.izmailov@nicevt.ru>
2023-10-30 16:35:02 +0300

    improve 28

--- problem28.py
+++ problem28.py
@@ -3,11 +3,12 @@
 def get_spiral_item(n):
     a,b,c,d = 1,1,1,1
     acc = 1
-    for i in range(n>>1):
-        a = d + 2*(i+1)
-        b = a + 2*(i +1)
-        c = b + 2*(i + 1)
-        d = c + 2*(i + 1)
+    for i in range(1, (n>>1) +1):
+        k = i<<1
+        a = d + k
+        b = a + k
+        c = b + k
+        d = c + k
         acc += a+b+c+d
     return acc
 
