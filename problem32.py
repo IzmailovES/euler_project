8b2a1e89679d201ecbf6dfba3bf9d816d2a5d6db
Egor Izmailov <e.izmailov@nicevt.ru>
2023-11-02 12:42:58 +0300

    problem 32 done

--- problem32.py
+++ problem32.py
@@ -2,7 +2,7 @@
 
 import sympy
 import itertools
-
+import functools
 def test(s_num):
     # sirst part - probuct
     for p in [4]:
@@ -10,15 +10,20 @@ def test(s_num):
         others = s_num[p:]
         for i in [1,2]:
             if int(others[:i]) * int(others[i:]) == pp:
-                yield frozenset((pp, int(others[:i]), int(others[i:])))
+                #yield (pp, int(others[:i]), int(others[i:]))
+                return pp
+    return None
 
 
-l = []
+l = set()
 s_num1 = '123456789'
 nums = (int(''.join(x)) for x in itertools.permutations(s_num1))
 for n in nums:
-    l.append(list(test(str(n))))
+    k = test(str(n))
+    if k:
+        l.add(k)
 print(l)
+print(functools.reduce(lambda x,y: x+y , l),)
 exit(0)
 
 
