bca810b8cc476308fc082ed605f36ef3aa9d18fd
Egor Izmailov <e.izmailov@nicevt.ru>
2023-12-04 10:32:47 +0300

    probllem 74 add cache

--- problem74.py
+++ problem74.py
@@ -6,14 +6,19 @@ fs = {x:math.factorial(x) for x in range(10)}
 def eval(num):
     return sum([fs[int(x)] for x in str(num)])
 
-
+ch = dict()
 def chain_len(num):
+    c_num = num
     s = set()
     ret = 0
     while not num in s:
+        if num in ch:
+            ch[c_num] = ret+ch[num]
+            return ch[c_num]
         s.add(num)
         num = eval(num)
         ret += 1
+    ch[c_num] = ret
     return ret
 
 ret = 0
